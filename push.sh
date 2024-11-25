973c35c5adef00808dee2dd944debc2133861e10
mauvadao <mauvadao4g@gmail.com>
2024-11-25 20:06:22 -0300

    Update: 241125200622

--- push.sh
+++ push.sh
@@ -7,8 +7,12 @@ clear
 echo -e '\e[44;1;33mUPANDO PRO GITHUB\e[0m'
 echo
 
+echo -e '\e[1;34m'
 git config --global --add safe.directory /storage/emulated/0/MAUVADAO/MAUVADAO_GITHHUB/RepoDoMau
 
+echo -e '\e[1;33m'
 git add -A
 git commit -m "Update: $(date +%y%m%d%H%M%S)"
 git push
+echo -e '\e[0m'
+echo  -e '\e[1;37mTERMINADO\e[0m'
